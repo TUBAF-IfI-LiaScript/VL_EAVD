c761fc87a42a85c3e28e6ff88bfac209723d7006
Sebastian Zug <Sebastian.Zug@informatik.tu-freiberg.de>
2020-12-22 09:14:21 +0100

    Adapt LED example - completed constructor

--- examples/09_ObjekteII/LED/LED.ino
+++ examples/09_ObjekteII/LED/LED.ino
@@ -1,25 +1,33 @@
 #include "RGB_LED.h"
-RGB_LED rgbLed;
+
+PinName red = PB_4;
+PinName green = PB_3;
+PinName blue = PC_7;
+
+RGB_LED rgbLed(red, green, blue);
+
 uint8_t color[][3] = {
         {255, 0, 0},                // red
         {0, 255, 0},                // green
         {0, 0, 255},                // blue
-        {0, 0, 0},
+        {0, 0, 0},                  
         {255, 255, 0},
         {0, 255, 255},
         {255, 0, 255},
         {255, 255, 255}
     };
-    void setup(){
-    }
-    void loop(){
-        for(int i = 0; i< 8; ++i)
-        {
-            Serial.printf("Red: %d, Green: %d, Blue: %d\n", color[i][0], color[i][1], color[i][2]);
-            rgbLed.setColor(color[i][0], color[i][1], color[i][2]);
-            delay(1000);
-        }
-        Serial.println("Turn off");
-        rgbLed.turnOff();
-        delay(1000);
-    }
+
+void setup(){
+}
+
+void loop(){
+   for(int i = 0; i< 8; ++i)
+   {
+       Serial.printf("Red: %d, Green: %d, Blue: %d\n", color[i][0], color[i][1], color[i][2]);
+       rgbLed.setColor(color[i][0], color[i][1], color[i][2]);
+       delay(1000);
+   }
+   Serial.println("Turn off");
+   rgbLed.turnOff();
+   delay(1000);
+}
