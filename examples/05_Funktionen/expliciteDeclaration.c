82a7eed12a0322c20a460b4bcd531427fa1fa8b9
SebastianZug <sebastian.zug@informatik.tu-freiberg.de>
2021-12-08 15:03:54 +0100

    Add first version of L5

---
+++ examples/05_Funktionen/expliciteDeclaration.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  int i = foo();       // <- Aufruf der Funktion
+  printf("i=%d\n", i);
+  return 0;
+}
+
+//int foo(void){         // <- Definition der Funktion
+//   return 3;
+//}
