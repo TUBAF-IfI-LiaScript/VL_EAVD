cf48f6a9ce4d65099d1adec4414f2e7c90415255
Sebastian Zug <sebastian.zug@informatik.tu-freiberg.de>
2022-01-12 16:45:47 +0100

    new pc

--- examples/05_Funktionen/expliciteDeclaration.c
+++ examples/05_Funktionen/expliciteDeclaration.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 int main(void) {
   int i = foo();       // <- Aufruf der Funktion
@@ -6,6 +7,6 @@ int main(void) {
   return 0;
 }
 
-//int foo(void){         // <- Definition der Funktion
-//   return 3;
-//}
+int foo(void){         // <- Definition der Funktion
+  return 3;
+}
